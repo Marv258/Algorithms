1c3dba46e79f614298d801f6aad863850f77f4b5
DE\AME2RT <Marvin.Maier@bosch-sensortec.com>
2023-10-18 14:23:51 +0200

    change func boyer_Moore, add comments

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def Boyer_Moore(_text: str, _pattern: str) -> list[str]:
+# Boyer-Moore-Algo
+# Horspool-Algo
+# Sunday-Algo
+# Knuth-Pratt-Moore-Algo
+def Boyer_Moore(_text: str, _pattern: str, only_first: bool = True) -> list[str]:
+    length_text: int = len(_text)
+    length_pattern: int = len(_pattern)
+    cursor = 0
+    while cursor < length_text
+
     pass
 
 
@@ -13,13 +22,13 @@ def index_in_list(_char: str, _list: list[str]) -> int:
     return _list.index(_char)
 
 
+
 if __name__ == "__main__":
     print("Main")
     text: str = "Hoola-Hoola girls like Hooligans"
-    pattern: str = "Hooligans"
+    pattern: str = "Hooligan"
 
     text_list: list[str] = string2list(text)
     pattern_list: list[str] = string2list(pattern)
 
-        
 
